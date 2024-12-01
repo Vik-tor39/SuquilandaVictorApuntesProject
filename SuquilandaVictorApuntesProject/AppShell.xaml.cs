c47691c331a44407b512b7111b47e76f51d38f4b
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2024-12-01 08:17:34 -0500

    Commit 1: Se agrego página About y TabBar

---
+++ SuquilandaVictorApuntesProject/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SuquilandaVictorApuntesProject
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
