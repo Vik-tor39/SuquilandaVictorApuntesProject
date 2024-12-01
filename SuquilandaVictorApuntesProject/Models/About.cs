0a13fbeb747fbdf44b9f78340654970bf0bada4c
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2024-12-01 13:31:34 -0500

    Commit 3: Se abstrayeron los modelos y vistas en distintos directorios. Se establecieron las funcionalidades para el enlazado Binding. Además se personalizo la vista AboutPage.

---
+++ SuquilandaVictorApuntesProject/Models/About.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuquilandaVictorApuntesProject.Models
+{
+    internal class About
+    {
+        public string Title => "Víctor Suquilanda";
+        public string Version => "A00103077";
+        public string MoreInfoUrl => "https://github.com/Vik-tor39/SuquilandaVictorApuntesProject.git";
+        public string Message => "Hola mundo, me llamo Víctor Suquilanda. Tengo 23 años, estudio la carrera de Ing. de Software en la UDLA.\n" +
+            "Y este es un proyecto .NET para la materia de Programación IV.";
+    }
+}
