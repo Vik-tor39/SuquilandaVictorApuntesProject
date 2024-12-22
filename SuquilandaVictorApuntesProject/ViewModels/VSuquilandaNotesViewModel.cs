c6684f31c81b47eb7c9fbfead7dee718fafe0da3
Victor Suquilanda <victor.suquilanda@udla.edu.ec>
2024-12-21 21:04:41 -0500

    Se modifico el proyecto de tal manera que se acople al patron MVVM, se empleo el tutorial.

---
+++ SuquilandaVictorApuntesProject/ViewModels/VSuquilandaNotesViewModel.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using SuquilandaVictorApuntesProject.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace SuquilandaVictorApuntesProject.ViewModels
+{
+    internal class VSuquilandaNotesViewModel : IQueryAttributable
+    {
+        public ObservableCollection<ViewModels.VSuquilandaNoteViewModel> AllNotes { get; }
+        public ICommand NewCommand { get; }
+        public ICommand SelectNoteCommand { get; }
+        public VSuquilandaNotesViewModel()
+        {
+            AllNotes = new ObservableCollection<ViewModels.VSuquilandaNoteViewModel>(Models.Note.LoadAll().Select(n => new VSuquilandaNoteViewModel(n)));
+            NewCommand = new AsyncRelayCommand(NewNoteAsync);
+            SelectNoteCommand = new AsyncRelayCommand<ViewModels.VSuquilandaNoteViewModel>(SelectNoteAsync);
+        }
+        private async Task NewNoteAsync()
+        {
+            await Shell.Current.GoToAsync(nameof(Views.NotePage));
+        }
+
+        private async Task SelectNoteAsync(ViewModels.VSuquilandaNoteViewModel note)
+        {
+            if (note != null)
+                await Shell.Current.GoToAsync($"{nameof(Views.NotePage)}?load={note.Identifier}");
+        }
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            if (query.ContainsKey("deleted"))
+            {
+                string noteId = query["deleted"].ToString();
+                VSuquilandaNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note exists, delete it
+                if (matchedNote != null)
+                    AllNotes.Remove(matchedNote);
+            }
+            else if (query.ContainsKey("saved"))
+            {
+                string noteId = query["saved"].ToString();
+                VSuquilandaNoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+                // If note is found, update it
+                if (matchedNote != null)
+                {
+                    matchedNote.Reload();
+                    //AllNotes.Move(AllNotes.IndexOf(matchedNote), 0);
+                }
+                
+                // If note isn't found, it's new; add it.
+                else
+                    AllNotes.Add(new VSuquilandaNoteViewModel(Note.Load(noteId)));
+            }
+        }
+    }
+}
--- SuquilandaVictorApuntesProject/obj/Debug/net9.0-android/android/src/mono/MonoPackageManager_Resources.java
+++ SuquilandaVictorApuntesProject/obj/Debug/net9.0-android/android/src/mono/MonoPackageManager_Resources.java
@@ -3,6 +3,7 @@ public class MonoPackageManager_Resources {
 	public static String[] Assemblies = new String[]{
 		/* We need to ensure that "SuquilandaVictorApuntesProject.dll" comes first in this list. */
 		"SuquilandaVictorApuntesProject.dll",
+		"CommunityToolkit.Mvvm.dll",
 		"GoogleGson.dll",
 		"Microsoft.Extensions.Configuration.dll",
 		"Microsoft.Extensions.Configuration.Abstractions.dll",
